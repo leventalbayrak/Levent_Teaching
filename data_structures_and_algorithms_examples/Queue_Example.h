439338e544659b9ee0c78ebcaa7b1f39e49d3239
levent <lealbayr@utmb.edu>
2018-09-16 10:45:40 -0500

    a

--- data_structures_and_algorithms_examples/Queue_Example.h
+++ data_structures_and_algorithms_examples/Queue_Example.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#pragma warning(disable:4996)
 
 namespace Queue
 {
--- data_structures_and_algorithms_examples/Stack_Example.h
+++ data_structures_and_algorithms_examples/Stack_Example.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#pragma warning(disable:4996)
+
 namespace Stack
 {
 	struct Stack
