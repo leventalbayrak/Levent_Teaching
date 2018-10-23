fb231f607085be9a19c4462e97439260cc68cf67
leventalbayrak <leventalbayrak@users.noreply.github.com>
2018-10-23 15:58:28 -0500

    f

--- data_structures_and_algorithms_examples/Heap_Example.h
+++ data_structures_and_algorithms_examples/Heap_Example.h
@@ -3,115 +3,94 @@ Data structures and algorithms
 Levent Albayrak 2018
 */
 #pragma once
+#include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-#pragma warning(disable:4996)
-
+#include <iostream>
 namespace Min_Heap
 {
 	struct Min_Heap
 	{
 		int *priority;
+		int *data;
+
 		int n_data;
-		int size;
+		int array_size;
 	};
 
-	void init(Min_Heap* h, int size)
+	void init(Min_Heap *h, int size)
 	{
 		h->n_data = 0;
-		h->size = size;
-		h->priority = (int*)malloc(sizeof(int)*h->size); assert(h->priority);
+		h->array_size = size;
+		h->priority = (int*)malloc(sizeof(int)*h->array_size);
+		h->data = (int*)malloc(sizeof(int)*h->array_size);
 	}
 
 	void resize(Min_Heap *h)
 	{
-		h->size <<= 1;
-		h->priority = (int*)realloc(h->priority, sizeof(int)*h->size); assert(h->priority);
+		h->array_size <<= 1;
+		h->priority = (int*)realloc(h->priority, sizeof(int)*h->array_size);
+		h->data = (int*)realloc(h->data, sizeof(int)*h->array_size);
 	}
 
-	void add(Min_Heap *h, int priority)
+	void add(Min_Heap *h, int priority, int data)
 	{
-		if (h->n_data >= h->size) resize(h);
+		if (h->n_data >= h->array_size) resize(h);
 
-		h->priority[h->n_data] = priority;
-
-		int current_index = h->n_data;
+		int current_index = h->n_data++;
+		h->priority[current_index] = priority;
+		h->data[current_index] = data;
 
 		for (;;)
 		{
 			if (current_index <= 0) break;
+			int parent_index = (current_index - 1) / 2;
+			if (h->priority[current_index] >= h->priority[parent_index]) break;
 
-			int parent_index = (current_index - 1) >> 1;
-			if (h->priority[current_index] < h->priority[parent_index])
-			{
-				//swap child priority with parent priority
-				int tmp = h->priority[current_index];
-				h->priority[current_index] = h->priority[parent_index];
-				h->priority[parent_index] = tmp;
-				
-				current_index = parent_index;
-			}
-			else
-			{
-				break;
-			}
+			int tmp_p = h->priority[current_index];
+			h->priority[current_index] = h->priority[parent_index];
+			h->priority[parent_index] = tmp_p;
+
+			int tmp_d = h->data[current_index];
+			h->data[current_index] = h->data[parent_index];
+			h->data[parent_index] = tmp_d;
+
+			current_index = parent_index;
 		}
-
-		h->n_data++;
 	}
 
-	int remove(Min_Heap *h)
+	void remove(int &priority, int &data, Min_Heap *h)
 	{
-		int root_priority = h->priority[0];
+		priority = h->priority[0];
+		data = h->data[0];
 
 		h->priority[0] = h->priority[h->n_data - 1];
-
+		h->data[0] = h->data[h->n_data - 1];
+		h->n_data--;
 		int current_index = 0;
 
 		for (;;)
 		{
-			if (current_index >= h->n_data) break;
 
-			int left_child_index = current_index * 2 + 1;
+			int child_index = 2 * current_index + 1;
+			if (child_index >= h->n_data) break;
 
-			if (left_child_index >= h->n_data) break;
-
-			int right_child_index = left_child_index + 1;
-
-			int better_child_index;
-
-			if (right_child_index >= h->n_data)
+			if (child_index + 1 < h->n_data && h->priority[child_index] >= h->priority[child_index + 1])
 			{
-				better_child_index = left_child_index;
-			}
-			else
-			{
-				better_child_index = left_child_index;
-				if (h->priority[right_child_index] < h->priority[better_child_index])
-				{
-					better_child_index = right_child_index;
-				}
+				child_index++;
 			}
 
-			if (h->priority[current_index] > h->priority[better_child_index])
-			{
-				//swap child priority with parent priority
-				int tmp = h->priority[current_index];
-				h->priority[current_index] = h->priority[better_child_index];
-				h->priority[better_child_index] = tmp;
+			if (h->priority[current_index] <= h->priority[child_index]) break;
 
-				current_index = better_child_index;
-			}
-			else
-			{
-				break;
-			}
-			
+			int tmp_p = h->priority[current_index];
+			h->priority[current_index] = h->priority[child_index];
+			h->priority[child_index] = tmp_p;
+
+			int tmp_d = h->data[current_index];
+			h->data[current_index] = h->data[child_index];
+			h->data[child_index] = tmp_d;
+
+			current_index = child_index;
+
 		}
-
-		h->n_data--;
-		return root_priority;
 	}
-
 }
