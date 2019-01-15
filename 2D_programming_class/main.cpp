d50c9f7750311c08185c6f5fa9763dbe235af0f6
leventalbayrak <leventalbayrak@users.noreply.github.com>
2019-01-15 08:49:15 -0600

    2d programming class

---
+++ 2D_programming_class/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+//include SDL header
+#include "SDL2-2.0.8\include\SDL.h"
+#include "SDL2-2.0.8\include\SDL_image.h"
+
+//load libraries
+#pragma comment(lib,"SDL2-2.0.8\\lib\\x86\\SDL2.lib")
+#pragma comment(lib,"SDL2-2.0.8\\lib\\x86\\SDL2main.lib")
+#pragma comment(lib,"SDL2-2.0.8\\lib\\x86\\SDL2_image.lib")
+
+#pragma comment(linker,"/subsystem:console")
+
+SDL_Renderer *renderer = NULL;
+int screen_width = 800;
+int screen_height = 600;
+
+unsigned char prev_key_state[256];
+unsigned char *keys = NULL;
+SDL_Window *window = NULL;
+
+int main(int argc, char **argv)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	prev_key_state[256];
+	keys = (unsigned char*)SDL_GetKeyboardState(NULL);
+
+	window = SDL_CreateWindow(
+		"WONDERFUL",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		screen_width, screen_height, SDL_WINDOW_SHOWN);
+
+	renderer = SDL_CreateRenderer(window,
+		-1, SDL_RENDERER_SOFTWARE);
+
+	SDL_Surface *your_draw_buffer = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 24, SDL_PIXELFORMAT_RGB24);
+	SDL_Surface *screen = SDL_GetWindowSurface(window);
+
+	for(;;)
+	{
+		memcpy(prev_key_state, keys, 256);
+
+		//consume all window events first
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				exit(0);
+			}
+		}
+
+		
+		//BLIT BUFFER TO SCREEN
+		SDL_BlitScaled(your_draw_buffer, NULL, screen, NULL);
+		SDL_UpdateWindowSurface(window);
+	}
+
+	return 0;
+}
