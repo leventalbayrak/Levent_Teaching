4279f5eb619e6827a56bbc79deda3d6d9478b1dd
leventalbayrak <leventalbayrak@users.noreply.github.com>
2019-01-23 13:12:07 -0600

    well

--- 2D_programming_class/main.cpp
+++ 2D_programming_class/main.cpp
@@ -15,25 +15,27 @@ using namespace std;
 
 
 
-void set_Pixel(unsigned char* buffer, int width, int x, int y, int r, int g, int b)
+void set_Pixel(unsigned char* buffer, int width, int x, int y, int r, int g, int b, int a)
 {
-	int first_byte_of_the_pixel = y * width * 3 + x * 3;
+	int first_byte_of_the_pixel = y * width * 4 + x * 4;
 	buffer[first_byte_of_the_pixel] = r;
 	buffer[first_byte_of_the_pixel + 1] = g;
 	buffer[first_byte_of_the_pixel + 2] = b;
+	buffer[first_byte_of_the_pixel + 3] = a;
 }
 
 //Alternative example
 struct Pixel
 {
-	unsigned char r, g, b;
+	unsigned char r, g, b, a;
 };
-void set_Pixel_Alternative(unsigned char* buffer, int width, int x, int y, int r, int g, int b)
+void set_Pixel_Alternative(unsigned char* buffer, int width, int x, int y, int r, int g, int b, int a)
 {
-	Pixel *p = (Pixel*) buffer;
+	Pixel *p = (Pixel*)buffer;
 	p[y*width + x].r = r;
 	p[y*width + x].g = g;
 	p[y*width + x].b = b;
+	p[y*width + x].a = a;
 }
 
 void fill_Rectangle(unsigned char*buffer, int buffer_width, int buffer_height, int rect_x, int rect_y, int rect_w, int rect_h, int r, int g, int b)
@@ -65,12 +67,14 @@ int main(int argc, char **argv)
 	renderer = SDL_CreateRenderer(window,
 		-1, SDL_RENDERER_SOFTWARE);
 
-	SDL_Surface *your_draw_buffer = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 24, SDL_PIXELFORMAT_RGB24);
+	SDL_Surface *your_draw_buffer = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 32, SDL_PIXELFORMAT_RGBA32);
 	SDL_Surface *screen = SDL_GetWindowSurface(window);
 
-	unsigned char *my_own_buffer = (unsigned char*)malloc(sizeof(unsigned char)*screen_width*screen_height * 3);
+	SDL_SetSurfaceBlendMode(your_draw_buffer, SDL_BLENDMODE_NONE);
 
-	for(;;)
+	unsigned char *my_own_buffer = (unsigned char*)malloc(sizeof(unsigned char)*screen_width*screen_height * 4);
+
+	for (;;)
 	{
 		memcpy(prev_key_state, keys, 256);
 
@@ -86,13 +90,14 @@ int main(int argc, char **argv)
 
 		for (int i = 0; i < screen_width*screen_height; i++)
 		{
-			my_own_buffer[i * 3] = rand() % 256;
-			my_own_buffer[i * 3 + 1] = 0;// rand() % 256;
-			my_own_buffer[i * 3 + 2] = rand() % 256;
+			my_own_buffer[i * 4] = rand() % 256;
+			my_own_buffer[i * 4 + 1] = rand() % 256;
+			my_own_buffer[i * 4 + 2] = rand() % 256;
+			my_own_buffer[i * 4 + 3] = 10;
 		}
-		
-		memcpy(your_draw_buffer->pixels, my_own_buffer, sizeof(unsigned char)*screen_width*screen_height * 3);
-		
+
+		memcpy(your_draw_buffer->pixels, my_own_buffer, sizeof(unsigned char)*screen_width*screen_height * 4);
+
 		//BLIT BUFFER TO SCREEN
 		SDL_BlitScaled(your_draw_buffer, NULL, screen, NULL);
 		SDL_UpdateWindowSurface(window);
--- 2D_programming_class/main_assignment_1.cpp
+++ 2D_programming_class/main_assignment_1.cpp
@@ -15,18 +15,19 @@ using namespace std;
 
 
 
-void set_Pixel(unsigned char* buffer, int width, int x, int y, int r, int g, int b)
+void set_Pixel(unsigned char* buffer, int width, int x, int y, int r, int g, int b, int a)
 {
-	int first_byte_of_the_pixel = y * width * 3 + x * 3;
+	int first_byte_of_the_pixel = y * width * 4 + x * 4;
 	buffer[first_byte_of_the_pixel] = r;
 	buffer[first_byte_of_the_pixel + 1] = g;
 	buffer[first_byte_of_the_pixel + 2] = b;
+	buffer[first_byte_of_the_pixel + 3] = a;
 }
 
 //Alternative example
 struct Pixel
 {
-	unsigned char r, g, b;
+	unsigned char r, g, b, a;
 };
 void set_Pixel_Alternative(unsigned char* buffer, int width, int x, int y, int r, int g, int b)
 {
@@ -34,6 +35,7 @@ void set_Pixel_Alternative(unsigned char* buffer, int width, int x, int y, int r
 	p[y*width + x].r = r;
 	p[y*width + x].g = g;
 	p[y*width + x].b = b;
+	p[y*width + x].a = a;
 }
 
 void fill_Rectangle(unsigned char*buffer, int buffer_width, int buffer_height, int rect_x, int rect_y, int rect_w, int rect_h, int r, int g, int b)
@@ -65,10 +67,10 @@ int main(int argc, char **argv)
 	renderer = SDL_CreateRenderer(window,
 		-1, SDL_RENDERER_SOFTWARE);
 
-	SDL_Surface *your_draw_buffer = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 24, SDL_PIXELFORMAT_RGB24);
+	SDL_Surface *your_draw_buffer = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 32, SDL_PIXELFORMAT_RGBA32);
 	SDL_Surface *screen = SDL_GetWindowSurface(window);
 
-	unsigned char *my_own_buffer = (unsigned char*)malloc(sizeof(unsigned char)*screen_width*screen_height * 3);
+	unsigned char *my_own_buffer = (unsigned char*)malloc(sizeof(unsigned char)*screen_width*screen_height * 4);
 
 	for(;;)
 	{
@@ -89,9 +91,10 @@ int main(int argc, char **argv)
 			my_own_buffer[i * 3] = rand() % 256;
 			my_own_buffer[i * 3 + 1] = 0;// rand() % 256;
 			my_own_buffer[i * 3 + 2] = rand() % 256;
+			my_own_buffer[i * 3 + 3] = 255;
 		}
 		
-		memcpy(your_draw_buffer->pixels, my_own_buffer, sizeof(unsigned char)*screen_width*screen_height * 3);
+		memcpy(your_draw_buffer->pixels, my_own_buffer, sizeof(unsigned char)*screen_width*screen_height * 4);
 		
 		//BLIT BUFFER TO SCREEN
 		SDL_BlitScaled(your_draw_buffer, NULL, screen, NULL);
