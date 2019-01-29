e594ab6b98a922a901d58c6d1a7d115cf2dd4379
leventalbayrak <leventalbayrak@users.noreply.github.com>
2019-01-29 10:25:16 -0600

    example

--- 2D_programming_class/rect.h
+++ 2D_programming_class/rect.h
@@ -1,24 +1,26 @@
 #pragma once
+
 #include "draw.h"
 
 struct Rect
 {
 	ColorSet color;
-	double x, y, w, h;
+	float x, y;
+	int w, h;
 };
 
-void fillRect(Rect rect, int screenW, int screenH, ColorSet *grid)
+void fill_Rect(Rect rect, int screen_w, int screen_H, ColorSet *grid)
 {
 	if (rect.x < 0) rect.x = 0;
-	if (rect.x + rect.w > screenW) rect.w = screenW - rect.x - 1;
+	if (rect.x + rect.w > screen_w) rect.w = screen_w - rect.x - 1;
 	if (rect.y < 0) rect.y = 0;
-	if (rect.y + rect.h > screenH) rect.h = screenH - rect.y - 1;
+	if (rect.y + rect.h > screen_H) rect.h = screen_H - rect.y - 1;
 
 	for (int i = rect.y; i < rect.y + rect.h; i++)
 	{
 		for (int j = rect.x; j < rect.x + rect.w; j++)
 		{
-			draw(&grid[j + (i * screenW)], rect.color);
+			draw(&grid[j + (i * screen_w)], rect.color);
 		}
 	}
 }
